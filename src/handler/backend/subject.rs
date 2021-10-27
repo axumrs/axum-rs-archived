3f29420281c4d02eb08f870152912a369cf371b1
axumrs <rolson867@gmail.com>
2021-10-27 17:09:50 +0800

    后台专题管理:处理处理

--- src/handler/backend/subject.rs
+++ src/handler/backend/subject.rs
@@ -1,18 +1,15 @@
-use askama::Template;
 use axum::{
     extract::{Extension, Form, Path, Query},
     http::{HeaderMap, StatusCode},
     response::Html,
 };
-use deadpool_postgres::Client;
 
 use crate::{
     arg,
     db::subject,
-    error::AppError,
     form,
     handler::{
-        helper::{get_client, log_error},
+        helper::{get_client, log_error, render},
         redirect::redirect,
     },
     html::backend::subject::{AddTemplate, EditTemplate, IndexTemplate},
@@ -25,7 +22,8 @@ pub async fn index(
     Extension(state): Extension<Arc<AppState>>,
     args: Option<Query<arg::SubjectBackendQueryArg>>,
 ) -> Result<Html<String>> {
-    let client: Client = get_client(state, "backend_subject_index").await?;
+    let handler_name = "backend_subject_index";
+    let client = get_client(state, handler_name).await?;
     let args = args.unwrap().0;
     let q_keyword = format!("%{}%", args.keyword());
     let subject_list = subject::select(
@@ -35,65 +33,70 @@ pub async fn index(
         args.page.unwrap_or(0),
     )
     .await
-    .map_err(|err| {
-        tracing::debug!("{:?}", err);
-        err
-    })?;
+    .map_err(log_error(handler_name.to_string()))?;
     let tmpl = IndexTemplate {
         arg: args,
         subject_list,
     };
-    let out = tmpl.render().map_err(|err| {
-        tracing::debug!("{:?}", err);
-        AppError::tmpl_error(err)
-    })?;
-    Ok(Html(out))
+    render(tmpl, handler_name)
 }
 pub async fn add() -> Result<Html<String>> {
     let tmpl = AddTemplate {};
-    let out = tmpl.render().map_err(AppError::tmpl_error)?;
-    Ok(Html(out))
+    render(tmpl, "backend_subject_add")
 }
 pub async fn add_action(
     Extension(state): Extension<Arc<AppState>>,
     form: Form<form::CreateSubject>,
 ) -> Result<(StatusCode, HeaderMap, ())> {
-    tracing::debug!("{:?}", form);
-    let client: Client = state.pool.get().await.map_err(AppError::from)?;
-    subject::create(&client, &form).await?;
+    let handler_name = "backend_subject_add_action";
+    let client = get_client(state, handler_name).await?;
+    subject::create(&client, &form)
+        .await
+        .map_err(log_error(handler_name.to_string()))?;
     redirect("/admin/subject?msg=专题添加成功")
 }
 pub async fn edit(
     Extension(state): Extension<Arc<AppState>>,
     Path(id): Path<i32>,
 ) -> Result<Html<String>> {
-    let client: Client = state.pool.get().await.map_err(AppError::from)?;
-    let sub = subject::find(&client, Some("id=$1"), &[&id]).await?;
+    let handler_name = "backend_subject_edit";
+    let client = get_client(state, handler_name).await?;
+    let sub = subject::find(&client, Some("id=$1"), &[&id])
+        .await
+        .map_err(log_error(handler_name.to_string()))?;
     let tmpl = EditTemplate { subject: sub };
-    let out = tmpl.render().map_err(AppError::tmpl_error)?;
-    Ok(Html(out))
+    render(tmpl, handler_name)
 }
 pub async fn edit_action(
     Extension(state): Extension<Arc<AppState>>,
     form: Form<form::UpdateSubject>,
 ) -> Result<(StatusCode, HeaderMap, ())> {
-    let client: Client = state.pool.get().await.map_err(AppError::from)?;
-    subject::update(&client, &form).await?;
+    let handler_name = "backend_subject_edit_action";
+    let client = get_client(state, handler_name).await?;
+    subject::update(&client, &form)
+        .await
+        .map_err(log_error(handler_name.to_string()))?;
     redirect("/admin/subject?msg=专题修改成功")
 }
 pub async fn del(
     Extension(state): Extension<Arc<AppState>>,
     Path(id): Path<i32>,
 ) -> Result<(StatusCode, HeaderMap, ())> {
-    let client: Client = state.pool.get().await.map_err(AppError::from)?;
-    subject::delete(&client, id).await?;
+    let handler_name = "backend_subject_del";
+    let client = get_client(state, handler_name).await?;
+    subject::delete(&client, id)
+        .await
+        .map_err(log_error(handler_name.to_string()))?;
     redirect("/admin/subject?msg=专题删除成功")
 }
 pub async fn restore(
     Extension(state): Extension<Arc<AppState>>,
     Path(id): Path<i32>,
 ) -> Result<(StatusCode, HeaderMap, ())> {
-    let client: Client = state.pool.get().await.map_err(AppError::from)?;
-    subject::restore(&client, id).await?;
-    redirect("/admin/subject?msg=专题恢复成功")
+    let handler_name = "backend_subject_restore";
+    let client = get_client(state, handler_name).await?;
+    subject::restore(&client, id)
+        .await
+        .map_err(log_error(handler_name.to_string()))?;
+    redirect("/admin/subject?msg=专题删除成功")
 }
--- src/handler/helper.rs
+++ src/handler/helper.rs
@@ -3,18 +3,28 @@ use std::sync::Arc;
 use crate::error::AppError;
 use crate::model::AppState;
 use crate::Result;
+use askama::Template;
+use axum::response::Html;
 use deadpool_postgres::Client;
 
-pub async fn get_client(state: Arc<AppState>, hander_name: &str) -> Result<Client> {
+pub async fn get_client(state: Arc<AppState>, handler_name: &str) -> Result<Client> {
     state.pool.get().await.map_err(|err| {
-        tracing::error!("无法获取数据库连接：{},  {}", err, hander_name);
+        tracing::error!("无法获取数据库连接：{:?},  {}", err, handler_name);
         AppError::from(err)
     })
 }
 
-pub async fn log_error(hander_name: &'static str) -> Box<dyn Fn(AppError) -> AppError> {
+pub fn log_error(handler_name: String) -> Box<dyn Fn(AppError) -> AppError> {
     Box::new(move |err| {
-        tracing::error!("操作失败：{},  {}", err, hander_name);
-        AppError::from(err)
+        tracing::error!("操作失败：{:?},  {}", err, handler_name);
+        err
     })
+}
+
+pub fn render<T: Template>(tmpl: T, handler_name: &str) -> Result<Html<String>> {
+    let out = tmpl.render().map_err(|err| {
+        tracing::error!("模板渲染出错：{:?}, {}", err, handler_name);
+        AppError::from(err)
+    })?;
+    Ok(Html(out))
 }
