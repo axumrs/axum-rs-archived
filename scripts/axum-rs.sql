a56aeeab51df53fbc9c8aecd67a6e84f233358a5
axumrs <rolson867@gmail.com>
2021-11-12 21:45:58 +0800

    修复:视图缺少字段

--- scripts/axum-rs.sql
+++ scripts/axum-rs.sql
@@ -98,10 +98,9 @@ LEFT JOIN (
 
 -- 前台专题的文章列表
 CREATE VIEW v_subject_topics AS
-SELECT v.id,v.title,v.slug,subject_slug,tag_names, LEFT(c.md,200) AS summary FROM v_topic_subject_list_with_tags AS v
+SELECT v.id,v.title,v.slug,subject_slug,tag_names,subject_name, LEFT(c.md,200) AS summary FROM v_topic_subject_list_with_tags AS v
 INNER JOIN topic_content AS c ON c.topic_id=v.id
 WHERE v.is_del=false AND v.subject_is_del=false;
-
 -- 前台文章详情
 CREATE VIEW v_topic_detail AS
 SELECT 
